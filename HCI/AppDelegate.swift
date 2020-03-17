4b529be8267cce9022ada5bbabd3580e000e3eb4
luthfifr <luthfir272@gmail.com>
2020-03-17 21:21:13 +0700

    setting up AppDelegate and SceneDelegate to be able to run in any iOS version

--- HCI/AppDelegate.swift
+++ HCI/AppDelegate.swift
@@ -11,15 +11,26 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        if #available(iOS 13, *) {
+            //skip anything here if iOS older than v13
+        } else {
+            let window = UIWindow()
+            let mainVC = HCMainViewController()
+            let navigation = UINavigationController(rootViewController: mainVC)
+            window.rootViewController = navigation
+            self.window = window
+            window.makeKeyAndVisible()
+        }
         return true
     }
+}
 
-    // MARK: UISceneSession Lifecycle
-
+// MARK: UISceneSession Lifecycle (iOS 13 and later)
+@available(iOS 13, *)
+extension AppDelegate {
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
@@ -31,7 +42,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
