7a19d0b4fa27da295a977a69f351924d49f8a1d2
luthfifr <luthfir272@gmail.com>
2020-03-17 18:36:43 +0700

    Initial Commit

---
+++ HCI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HCI
+//
+//  Created by Luthfi Fathur Rahman on 17/03/20.
+//  Copyright © 2020 Stand Alone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
