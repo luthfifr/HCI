4b222177715c9983235ec54199e060875802e139
luthfifr <luthfir272@gmail.com>
2020-03-17 22:35:13 +0700

    add tableView, HCProductTableViewCell and HCBlogTableViewCell

---
+++ HCI/Views/Main/HCBlogTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HCBlogTableViewCell.swift
+//  HCI
+//
+//  Created by Luthfi Fathur Rahman on 17/03/20.
+//  Copyright © 2020 Stand Alone. All rights reserved.
+//
+
+import UIKit
+
+class HCBlogTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- HCI/Views/Main/HCViewController.swift
+++ HCI/Views/Main/HCViewController.swift
@@ -7,11 +7,98 @@
 //
 
 import UIKit
+import RxSwift
+import SnapKit
 
 class HCMainViewController: UIViewController {
 
+    typealias ProductTVC = HCProductTableViewCell
+    typealias BlogTVC = HCBlogTableViewCell
+
+    private var tableView: UITableView!
+
+    private let prodSectionCellID = String(describing: ProductTVC.self)
+    private let blogSectionCellID = String(describing: BlogTVC.self)
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .gray
+        title = "Home Credit Indonesia"
+        view.backgroundColor = UIColor(red: 248, green: 248, blue: 248, alpha: 1.0)
+        setupViews()
+    }
+}
+
+// MARK: - Private methods
+extension HCMainViewController {
+    private func setupViews() {
+        setupTableView()
+    }
+
+    private func setupTableView() {
+        if tableView == nil {
+            tableView = UITableView(frame: .zero)
+            tableView.backgroundColor = .clear
+            tableView.allowsMultipleSelection = false
+            tableView.translatesAutoresizingMaskIntoConstraints = false
+            tableView.tableFooterView = UIView()
+            tableView.dataSource = self
+            tableView.delegate = self
+
+            tableView.register(ProductTVC.self, forCellReuseIdentifier: prodSectionCellID)
+            tableView.register(BlogTVC.self, forCellReuseIdentifier: blogSectionCellID)
+
+            if !view.subviews.contains(tableView) {
+                view.addSubview(tableView)
+            }
+
+            tableView.snp.makeConstraints({ make in
+                make.edges.equalToSuperview()
+            })
+        }
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension HCMainViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 363
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = UITableViewCell()
+
+        switch indexPath.section {
+        case 0:
+            guard let cell0 = tableView.dequeueReusableCell(withIdentifier: prodSectionCellID, for: indexPath) as? ProductTVC else { //swiftlint:disable:this line_length
+                return UITableViewCell()
+            }
+            cell0.selectionStyle = .none
+            cell = cell0
+        case 1:
+            guard let cell1 = tableView.dequeueReusableCell(withIdentifier: blogSectionCellID, for: indexPath) as? BlogTVC else { //swiftlint:disable:this line_length
+                return UITableViewCell()
+            }
+            cell1.selectionStyle = .blue
+            cell = cell1
+        default:
+            break
+        }
+
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension HCMainViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //codes here
     }
 }
